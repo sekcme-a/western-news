327b5f634ee226ee2dfc01fd29c7c0c7354d89b1
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2025-12-09 14:53:53 +0900

    asdf

--- app/auth/forgot-password/page.js
+++ app/auth/forgot-password/page.js
@@ -53,7 +53,8 @@ const ForgotPasswordPage = () => {
     try {
       // Supabase의 `resetPasswordForEmail` 함수 사용
       const { error } = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: `https://www.western-news.co.kr/auth/update-password`, // 이메일 링크 클릭 후 이동할 페이지
+        // redirectTo: `https://www.western-news.co.kr/auth/update-password`, // 이메일 링크 클릭 후 이동할 페이지
+        redirectTo: `https://www.western-news.co.kr/auth/callback`,
       });
 
       if (error) {
